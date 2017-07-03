52f94048174c3311f4c2601bee1eb449ffaee4e4
Yago Lopez <yamartin@mac-yago.local>
2017-07-03 08:42:46 +0200

    Initial Commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by Yago de Martin Lopez on 3/7/17.
+//  Copyright © 2017 Yago de Martin Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
